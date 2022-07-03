479485cb136cb542629f5fe95e8066e4651f4918
WalterBright <ssmbuild@outlook.com>
2022-07-04 00:50:02 +0800

    bug++

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+    outDir:'dist/walter-extension',
+  },
   plugins: [vue()],
   resolve: {
     alias: {
